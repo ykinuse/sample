e2396ab0abb24cc77c95352e5c11c3bfeb54f545
Yong Kai Teo <yongkai.teo@ITSG000476-MAC.local>
2017-08-29 15:56:25 +0800

    more

--- app/src/test/java/kai/sample/MergeKSortedArray.java
+++ app/src/test/java/kai/sample/MergeKSortedArray.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 /**
@@ -13,7 +14,7 @@ import java.util.TreeSet;
 public class MergeKSortedArray {
     @Test
     public void test() throws Exception {
-        System.out.println(Arrays.toString(mergeKSortedArray(new int[][]{new int[]{1, 4, 7}, new int[]{2, 5, 8}, new int[]{3, 6, 9}})));
+        System.out.println(Arrays.toString(mergeKSortedArray2(new int[][]{new int[]{2, 5, 8}, new int[]{1, 4, 7}, new int[]{3, 6, 9}})));
     }
 
     private Integer[] mergeKSortedArray(int[][] arr) {
@@ -25,5 +26,50 @@ public class MergeKSortedArray {
         }
 
         return new ArrayList<>(set).toArray(new Integer[set.size()]);
+    }
+
+    private int[] mergeKSortedArray2(int[][] arr) {
+        PriorityQueue<NodeQueue> nodeQueues = new PriorityQueue<>();
+        int size = 0;
+        for (int[] i : arr) {
+            size += i.length;
+            nodeQueues.add(new NodeQueue(i, 0, i[0]));
+        }
+
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            NodeQueue nodeQueue = nodeQueues.poll();
+            result[i] = nodeQueue.value;
+            if (nodeQueue.index + 1 < nodeQueue.arr.length) {
+                nodeQueues.add(new NodeQueue(nodeQueue.arr, nodeQueue.index + 1, nodeQueue.arr[nodeQueue.index + 1]));
+            }
+
+        }
+
+        return result;
+    }
+
+    class NodeQueue implements Comparable<NodeQueue> {
+        int[] arr;
+        int index;
+        int value;
+
+        public NodeQueue(int[] arr, int index, int value) {
+            this.arr = arr;
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(NodeQueue nodeQueue) {
+            if (nodeQueue.value > value) {
+                return -1;
+            }
+            if (nodeQueue.value < value) {
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
